a9552ea43fc21a98cc0587eaaf09494e7f0ad022
Kaito-Dogi <kendobu0405@gmail.com>
2026-01-12 20:40:06 +0900

    docs: KDoc を追加

--- shared/src/commonMain/kotlin/app/kaito_dogi/smopin/domain/smokingArea/Location.kt
+++ shared/src/commonMain/kotlin/app/kaito_dogi/smopin/domain/smokingArea/Location.kt
@@ -2,6 +2,12 @@ package app.kaito_dogi.smopin.domain.smokingArea
 
 import kotlinx.serialization.Serializable
 
+/**
+ * 緯度経度
+ *
+ * @param latitude 緯度
+ * @param longitude 経度
+ */
 @Serializable
 data class Location(
   val latitude: Latitude,
--- shared/src/commonMain/kotlin/app/kaito_dogi/smopin/domain/smokingArea/Longitude.kt
+++ shared/src/commonMain/kotlin/app/kaito_dogi/smopin/domain/smokingArea/Longitude.kt
@@ -2,6 +2,11 @@ package app.kaito_dogi.smopin.domain.smokingArea
 
 import kotlinx.serialization.Serializable
 
+/**
+ * 経度
+ *
+ * @param value 経度の値
+ */
 @Serializable
 data class Longitude(
   val value: Double,
