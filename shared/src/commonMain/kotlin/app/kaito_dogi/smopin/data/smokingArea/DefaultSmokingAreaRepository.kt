e922dffb826fd9114ed88aae1bed533d33170321
Kaito-Dogi <kendobu0405@gmail.com>
2026-01-12 21:00:15 +0900

    feat: SmokingAreaNetworkDataSource の Fake 実装を追加

--- shared/src/commonMain/kotlin/app/kaito_dogi/smopin/data/smokingArea/DefaultSmokingAreaRepository.kt
+++ shared/src/commonMain/kotlin/app/kaito_dogi/smopin/data/smokingArea/DefaultSmokingAreaRepository.kt
@@ -1,21 +1,13 @@
 package app.kaito_dogi.smopin.data.smokingArea
 
-import app.kaito_dogi.smopin.domain.smokingArea.Latitude
-import app.kaito_dogi.smopin.domain.smokingArea.Location
-import app.kaito_dogi.smopin.domain.smokingArea.Longitude
 import app.kaito_dogi.smopin.domain.smokingArea.SmokingArea
 import app.kaito_dogi.smopin.domain.smokingArea.SmokingAreaRepository
+import app.kaito_dogi.smopin.network.smokingArea.FakeSmokingAreaNetworkDataSource
 
-internal class DefaultSmokingAreaRepository : SmokingAreaRepository {
+internal class DefaultSmokingAreaRepository(
+  private val smokingAreaNetworkDataSource: FakeSmokingAreaNetworkDataSource,
+) : SmokingAreaRepository {
   override suspend fun getSmokingAreaList(): List<SmokingArea> {
-    return List(size = 3) {
-      SmokingArea(
-        name = "Mock Smoking Area $it",
-        location = Location(
-          latitude = Latitude(value = 35.6889544 + it * 0.1),
-          longitude = Longitude(value = 139.6992443 + it * 0.1),
-        ),
-      )
-    }
+    return smokingAreaNetworkDataSource.getSmokingAreaList()
   }
 }
