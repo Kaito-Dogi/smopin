c6012d493bb7140342d501b7e5118690df0da6d2
Kaito-Dogi <kendobu0405@gmail.com>
2026-04-25 20:59:12 +0900

    chore: 不要なコメントを削除

--- shared/data/src/commonMain/kotlin/app/kaito_dogi/smopin/shared/data/location/LocationDataSource.kt
+++ shared/data/src/commonMain/kotlin/app/kaito_dogi/smopin/shared/data/location/LocationDataSource.kt
@@ -3,7 +3,6 @@ package app.kaito_dogi.smopin.shared.data.location
 import kotlinx.coroutines.flow.Flow
 import kotlin.time.Duration
 
-// TODO: interface にデフォルト引数を渡せるかどうか確認する
 interface LocationDataSource {
   fun getCurrentLocationStream(
     isPreciseEnabled: Boolean,
